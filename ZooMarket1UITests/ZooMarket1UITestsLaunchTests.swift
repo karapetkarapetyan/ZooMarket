3c4cd48909bdff4214f08ec996215383f4d42dda
Karapet <karapet0910@gmail.com>
2022-05-24 20:37:09 +0400

    Added Git Ignore

---
+++ ZooMarket1UITests/ZooMarket1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ZooMarket1UITestsLaunchTests.swift
+//  ZooMarket1UITests
+//
+//  Created by Karapet on 24.05.22.
+//
+
+import XCTest
+
+class ZooMarket1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
